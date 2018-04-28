3d467eab7b8b08886683b2b3987e439a35770afd
Léo Lam <leo@innovatetechnologi.es>
2018-04-29 00:00:55 +0200

    Update

--- map.py
+++ map.py
@@ -25,6 +25,20 @@ for outfilename,folder_suffix in files:
         dropactor_objs = (root.findall('./*/value') +
                           root.findall('./Rails/*/RailPoints/value'))
 
+        # Build a list of actor instances that are excluded from the blood moon mechanism
+        # (they will instead respawn immediately after reloading)
+        off_wait_revival_actors = set()
+        for actor in dropactor_objs:
+            if 'LinkTag' not in actor.find('UnitConfigName').text:
+                continue
+            links = actor.findall('./LinksToObj/value')
+            if not links:
+                continue
+            for link in links:
+                link_type = link.find('DefinitionName').text
+                if link_type == 'OffWaitRevival':
+                    off_wait_revival_actors.add(link.get('DestUnitHashId'))
+
         for actor in dropactor_objs:
             name = actor.findall('./UnitConfigName')[0].text
             coords = [float(node.text[:-1]) for node in actor.findall('./Translate/value')]
@@ -46,30 +60,62 @@ for outfilename,folder_suffix in files:
                            actor.findall('./_Parameters/EquipItem4') +
                            actor.findall('./_Parameters/EquipItem5') +
                            actor.findall('./_Parameters/ArrowName'))
-            hard = actor.findall('./_Parameters[@IsHardModeActor]')
-            is_hardmode = False
-            if hard and hard[0].attrib['IsHardModeActor'] == 'true':
-                is_hardmode = True
-            
+
+            params = actor.find('_Parameters')
+            is_hardmode = params != None and params.get('IsHardModeActor', False)
+
             if name in object_names:
                 nice_name = object_names[name]
             else:
                 nice_name = name
-                
+
             if len(drop_tables):
                 drop_table = drop_tables[0].text
                 if drop_table != 'Normal':
                     name = name+':'+drop_table
                     nice_name = nice_name+':'+drop_table
             for drop_actor in drop_actors:
-                drop_actor = drop_actor.text
-                if drop_actor in ('Normal', 'Default', 'NormalArrow'):
+                if drop_actor.text in ('Normal', 'Default', 'NormalArrow'):
                     continue
-                name += ':' + drop_actor
-                nice_name += ':' + object_names[drop_actor]
+                name += ':' + drop_actor.text
+                nice_name += ':' + object_names[drop_actor.text]
             if is_hardmode:
                 name = 'HARD:'+name
                 nice_name = 'HARD:'+nice_name
+
+            if ('Enemy_' in name or 'Weapon_' in name) and ('TBox_' not in name):
+                level_sensor_mode = params != None and int(params.get('LevelSensorMode', 0))
+                if level_sensor_mode < 1:
+                    if 'Enemy_Giant_' in name:
+                        name += ':NO_SCALING_ENEMY'
+                        nice_name += ':No scaling (enemy)'
+                    else:
+                        name += ':NO_SCALING'
+                        nice_name += ':No scaling'
+
+            if 'Enemy_' in name:
+                disable_rankup = params != None and bool(params.get('DisableRankUpForHardMode', False))
+                if disable_rankup:
+                    name += ':NO_RANKUP'
+                    nice_name += ':No rankup'
+
+            if 'Weapon_' in name:
+                weapon_modifier = params != None and int(params.get('SharpWeaponJudgeType', 0))
+                if weapon_modifier != 0:
+                    name += ':MODIFIER_' + str(weapon_modifier)
+                    nice_name += ':'
+                    if weapon_modifier == 1:
+                        nice_name += '⭐(random)'
+                    if weapon_modifier == 2:
+                        nice_name += '⭐'
+                    if weapon_modifier == 3:
+                        nice_name += '⭐⭐'
+
+            actor_hash_id = actor.get('HashId')
+            if actor_hash_id in off_wait_revival_actors:
+                name += ':OFF_WAIT_REVIVAL'
+                nice_name += ':Always respawn'
+
             if name not in objects:
                 objects[name] = {'display_name':nice_name, 'locations':[]}
             objects[name]['locations'].append([round(coords[0],2), round(coords[-1],2)])
